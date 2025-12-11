f037f275507f56d71a61fc89bf849a1c280e2959
yujiezheng <yujie70338@gmail.com>
2025-12-12 01:41:55 +0800

    Refactor investment calculation to include user input for investment amount and years

--- Section-2/investment.go
+++ Section-2/investment.go
@@ -6,10 +6,22 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5   // const is immutable
+	var investmentAmount = 1000 // var is mutable
+	// var expectedReturnRate = 5.5
+	expectedReturnRate := 5.5 // shorter way to assign a variable
+	var years = 10.0
 
-	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
+	fmt.Print("input your investmentAmount : ")
+	fmt.Scan(&investmentAmount) // fmt.Scan(&investmentAmount) reads text from standard input (usually the keyboard), converts it to the appropriate type, and stores it into investmentAmount.
+
+	fmt.Print("input your invest year : ")
+	fmt.Scan(&years)
+
+	// var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
+	futureValue := float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
